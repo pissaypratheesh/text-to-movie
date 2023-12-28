952cb0b81ff3b7374b3c2ac1ab8dd1191091867b
s <pratheesh.pm@verse.in>
2023-12-28 23:25:03 +0530

    aider: Added fetching sentences from API and updating store in HomePage component.
    # Aider chat conversation:
    
    ASSISTANT: To fetch sentences from a network call and update the `sentences` variable in the `Store` class using `updateSentences`, we need to modify `nodejs/pages/index.js`:
    
    1. Import `useStore` from `mobx-react-lite`.
    2. Import `useEffect` and `useState` from `react`.
    3. Create a `store` instance using `useStore()`.
    4. Add a `useEffect` hook to fetch sentences and call `updateSentences`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    nodejs/pages/index.js
    ```javascript
    <<<<<<< SEARCH
    import React, { useEffect, useState } from 'react';
    =======
    import React, { useEffect, useState } from 'react';
    import { useStore } from 'mobx-react-lite';
    >>>>>>> REPLACE
    ```
    
    nodejs/pages/index.js
    ```javascript
    <<<<<<< SEARCH
    function HomePage() {
    =======
    function HomePage() {
      const store = useStore();
    >>>>>>> REPLACE
    ```
    
    nodejs/pages/index.js
    ```javascript
    <<<<<<< SEARCH
      return (
        <div>
          <AssetsAggregation />
        </div>
      );
    =======
      useEffect(() => {
        async function fetchSentences() {
          try {
            const response = await fetch('https://api.example.com/sentences');
            const data = await response.json();
            store.updateSentences(data.sentences);
          } catch (error) {
            console.error('Error fetching sentences:', error);
          }
        }
    
        fetchSentences();
      }, []);
    
      return (
        <div>
          <AssetsAggregation />
        </div>
      );
    >>>>>>> REPLACE
    ```
    
    Replace `https://api.example.com/sentences` with the actual API endpoint you want to fetch the sentences from. The code assumes the API returns a JSON object with a `sentences` property containing an array of sentences.

--- nodejs/pages/index.js
+++ nodejs/pages/index.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useStore } from 'mobx-react-lite';
 import AssetsAggregation from '../components/AssetsAggregation';
 var _ = require('underscore')
 
 function HomePage() {
+  const store = useStore();
+
+  useEffect(() => {
+    async function fetchSentences() {
+      try {
+        const response = await fetch('https://api.example.com/sentences');
+        const data = await response.json();
+        store.updateSentences(data.sentences);
+      } catch (error) {
+        console.error('Error fetching sentences:', error);
+      }
+    }
+
+    fetchSentences();
+  }, []);
 
   return (
     <div>
