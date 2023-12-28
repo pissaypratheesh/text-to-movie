945fac0a9b28d5b74c5b5f240814caadeef63bdf
s <pratheesh.pm@verse.in>
2023-12-28 23:29:26 +0530

    Refactored API call and added StoreProvider import in index.js

--- nodejs/pages/index.js
+++ nodejs/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useStore } from 'mobx-react-lite';
 import AssetsAggregation from '../components/AssetsAggregation';
+import { useStore } from '../components/StoreProvider'
 var _ = require('underscore')
 
 function HomePage() {
@@ -9,7 +10,7 @@ function HomePage() {
   useEffect(() => {
     async function fetchSentences() {
       try {
-        const response = await fetch('https://api.example.com/sentences');
+        const response = await fetch('/api/sentences');
         const data = await response.json();
         store.updateSentences(data.sentences);
       } catch (error) {
