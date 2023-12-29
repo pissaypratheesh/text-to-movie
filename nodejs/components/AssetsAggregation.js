4f9b69fbd9b768ac77c478231e7894ba4970d242
s <pratheesh.pm@verse.in>
2023-12-29 15:41:58 +0530

    Implemented file upload functionality in AssetsAggregation component

--- nodejs/components/AssetsAggregation.js
+++ nodejs/components/AssetsAggregation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ImageSelection from "./ImageSelection";
 import VideoSelection from "./VideoSelection";
 import { useStore } from "./StoreProvider";
@@ -9,15 +9,41 @@ import { useDropzone } from "react-dropzone";
 var _ = require("underscore");
 
 const AssetsAggregation = observer(function AssetsAggregation() {
+  const uploadFile = async (file) => {
+    // Replace this URL with your backend API endpoint
+    const apiUrl = "/upload";
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("File uploaded successfully");
+      } else {
+        console.error("Error uploading file");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
   const onDrop = useCallback((acceptedFiles) => {
-    // Handle the file upload here
-    console.log(acceptedFiles);
+    acceptedFiles.forEach((file) => {
+      uploadFile(file);
+    });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   const [query, setQuery] = useState("");
   const store = useStore();
   const { sentences } = store;
-  console.log("🚀 ~ file: AssetsAggregation.js:13 ~ AssetsAggregation ~ sentences:", toJS(sentences))
+  console.log(
+    "🚀 ~ file: AssetsAggregation.js:13 ~ AssetsAggregation ~ sentences:",
+    toJS(sentences)
+  );
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -40,12 +66,12 @@ const AssetsAggregation = observer(function AssetsAggregation() {
 
   return (
     <div className="m-4">
-
       {sentences.map((sentenceObj, index) => {
         let { line: sentence } = sentenceObj;
         let selectedImgs = toJS(sentenceObj.selectedImgs || []);
-        let selectedVids =  toJS(sentenceObj.selectedVids || []);
-        let isBothEmpty = selectedImgs.length === 0 && selectedVids.length === 0;
+        let selectedVids = toJS(sentenceObj.selectedVids || []);
+        let isBothEmpty =
+          selectedImgs.length === 0 && selectedVids.length === 0;
         return (
           <details key={index} className="mb-4">
             <summary
@@ -55,42 +81,47 @@ const AssetsAggregation = observer(function AssetsAggregation() {
               {sentence}
             </summary>
             {!isBothEmpty && (
-                <>
-                        {(<h2 className="text-2xl font-semibold mb-4">
-                            Selected Images and Videos
-                        </h2>)}
-                        <div
-                            className="grid grid-cols-3 gap-4 mb-8"
-                            key={`${selectedImgs.length}_${selectedVids.length}`}
-                        >
-                            {Object.values(selectedImgs).map((image, index) => (
-                                <img
-                                    key={index}
-                                    src={image.src}
-                                    alt="Selected"
-                                    className="w-full h-32 object-cover"
-                                />
-                            ))}
-                            {Object.values(selectedVids).map((video, index) => {
-                                    return (
-                                    <img
-                                        key={index}
-                                        src={video.src}
-                                        controls
-                                        className="w-full h-32 object-cover"
-                                    />
-                                    );
-                            })}
-                            <div {...getRootProps()} className="border-dashed border-2 border-gray-400 p-4 cursor-pointer">
-                              <input {...getInputProps()} />
-                              {
-                                isDragActive ?
-                                  <p>Drop the files here ...</p> :
-                                  <p>Click or drag a file to upload</p>
-                              }
-                            </div>
-                        </div>
-                </>
+              <>
+                {
+                  <h2 className="text-2xl font-semibold mb-4">
+                    Selected Images and Videos
+                  </h2>
+                }
+                <div
+                  className="grid grid-cols-3 gap-4 mb-8"
+                  key={`${selectedImgs.length}_${selectedVids.length}`}
+                >
+                  {Object.values(selectedImgs).map((image, index) => (
+                    <img
+                      key={index}
+                      src={image.src}
+                      alt="Selected"
+                      className="w-full h-32 object-cover"
+                    />
+                  ))}
+                  {Object.values(selectedVids).map((video, index) => {
+                    return (
+                      <img
+                        key={index}
+                        src={video.src}
+                        controls
+                        className="w-full h-32 object-cover"
+                      />
+                    );
+                  })}
+                  <div
+                    {...getRootProps()}
+                    className="border-dashed border-2 border-gray-400 p-4 cursor-pointer"
+                  >
+                    <input {...getInputProps()} />
+                    {isDragActive ? (
+                      <p>Drop the files here ...</p>
+                    ) : (
+                      <p>Click or drag a file to upload</p>
+                    )}
+                  </div>
+                </div>
+              </>
             )}
           </details>
         );
@@ -102,39 +133,44 @@ const AssetsAggregation = observer(function AssetsAggregation() {
             sentenceObj={selectedSentence}
             onImageSelect={(image, sentence, sentenceObj) => {
               let newSentences = [...store.sentences];
-              let newImageArr = newSentences[sentenceObj.index]['selectedImgs'] || [];
-      
-              //selectAll or deselct all    
+              let newImageArr =
+                newSentences[sentenceObj.index]["selectedImgs"] || [];
+
+              //selectAll or deselct all
               if (Array.isArray(image)) {
                 if (!image.length) {
                   newImageArr = [];
-                }else{
-                    newImageArr = image;
+                } else {
+                  newImageArr = image;
                 }
               } else {
                 // select/deselect single
                 if (image.isSelected) {
-                    newImageArr[newImageArr.length] = image;
+                  newImageArr[newImageArr.length] = image;
                 } else {
-                    newImageArr = newImageArr.filter(img => img.src != image.src)
+                  newImageArr = newImageArr.filter(
+                    (img) => img.src != image.src
+                  );
                 }
               }
               //Update store
-              newSentences[sentenceObj.index]['selectedImgs'] = newImageArr;
-              store.updateSentences(newSentences)
+              newSentences[sentenceObj.index]["selectedImgs"] = newImageArr;
+              store.updateSentences(newSentences);
             }}
           />
           <VideoSelection
             q={selectedSentence.line}
             sentenceObj={selectedSentence}
             onVideoSelect={(video, sentence, sentenceObj) => {
-                let newSentences = [...store.sentences];
-                if(video && Array.isArray(video)){
-                    video = video.map((v,i) => {return {...v, index: i}})
-                }
-                //let newVidArr = newSentences[sentenceObj.index]['selectedVids'] || [];
-                newSentences[sentenceObj.index]['selectedVids'] = video;
-                store.updateSentences(newSentences)
+              let newSentences = [...store.sentences];
+              if (video && Array.isArray(video)) {
+                video = video.map((v, i) => {
+                  return { ...v, index: i };
+                });
+              }
+              //let newVidArr = newSentences[sentenceObj.index]['selectedVids'] || [];
+              newSentences[sentenceObj.index]["selectedVids"] = video;
+              store.updateSentences(newSentences);
             }}
           />
         </div>
