561e7bd96090412762c6b03e24bc8f15df8a9535
s <pratheesh.pm@verse.in>
2024-01-04 12:00:01 +0530

    Extracted timings input into separate TimingsInput component

--- nodejs/components/VideoModal.js
+++ nodejs/components/VideoModal.js
@@ -149,25 +149,12 @@ const VideoModal = observer(function VideoModal({ showModal, handleClose, select
                       <>
                         <YouTubePlayer videoId={selectedVideo.videoId} onReady={onReady} />
 
-                        <div className="flex items-center mt-2">
-                          <input
-                            type="text"
-                            className="flex-grow p-2 border border-gray-300 rounded"
-                            placeholder="Enter selected timings like ['1-3','4-5'].."
-                            value={timingsText}
-                            onChange={(e) => setTimingsText(e.target.value)}
-                          />
-                          <button
-                            type="button"
-                            className="ml-2 px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700"
-                            onClick={validateTimingsText}
-                          >
-                            Submit
-                          </button>
-                        </div>
-                        {errorMessage && (
-                          <p className="text-red-600 mt-2">{errorMessage}</p>
-                        )}
+                        <TimingsInput                                                                                                                                                                                
+                           timingsText={timingsText}                                                                                                                                                                  
+                           setTimingsText={setTimingsText}                                                                                                                                                            
+                           validateTimingsText={validateTimingsText}                                                                                                                                                  
+                           errorMessage={errorMessage}                                                                                                                                                                
+                         />        
                         <input
                           type="text"
                           className="w-full p-2 border border-gray-300 rounded mt-4"
