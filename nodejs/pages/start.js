63c48c2c1af8b49e433225413ac5b53aa4981687
s <pratheesh.pm@verse.in>
2024-01-04 12:09:50 +0530

    Changed button text from "Fetch Data" to "Fetch"

--- nodejs/pages/start.js
+++ nodejs/pages/start.js
@@ -28,7 +28,7 @@ function Start() {
           className="bg-blue-500 text-white px-4 py-2 rounded ml-4"
           onClick={handleButtonClick}
         >
-          Fetch Data
+          Fetch
         </button>
       </div>
       <div className="response-data">
