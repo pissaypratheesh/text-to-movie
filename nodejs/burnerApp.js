5c1c822c1c36972e01a2801ca29d4f42fb33c984
s <pratheesh.pm@verse.in>
2024-01-12 11:27:04 +0530

    aider: Enabled CORS in burnerApp.js by adding cors middleware
    # Aider chat conversation:
    
    USER: enable cors in burnerApp.js
    
    ASSISTANT: To enable CORS in `burnerApp.js`, we need to:
    
    1. Install the `cors` package.
    2. Import the `cors` package in `burnerApp.js`.
    3. Use the `cors` middleware in the `burnerApp.js` file.
    
    Please run the following command in your terminal to install the `cors` package:
    
    ```
    npm install cors
    ```
    
    After installing the `cors` package, apply the following *SEARCH/REPLACE* blocks to modify `burnerApp.js`:
    
    nodejs/burnerApp.js
    ```javascript
    <<<<<<< SEARCH
    const bodyParser = require('body-parser');
    =======
    const bodyParser = require('body-parser');
    const cors = require('cors');
    >>>>>>> REPLACE
    ```
    
    nodejs/burnerApp.js
    ```javascript
    <<<<<<< SEARCH
    app.use(bodyParser.urlencoded({ extended: true }));
    =======
    app.use(bodyParser.urlencoded({ extended: true }));
    app.use(cors());
    >>>>>>> REPLACE
    ```
    
    These changes will enable CORS for your `burnerApp.js` server.

--- nodejs/burnerApp.js
+++ nodejs/burnerApp.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 const port = 9999;
 const path = require('path');
 const fs = require("fs");
