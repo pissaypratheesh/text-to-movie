8b89073c35b927aa2209b2d912f634cc08f12b71
s <pratheesh.pm@verse.in>
2024-01-12 09:05:19 +0530

    Added index.js to nodejs/utils directory

---
+++ nodejs/utils/index.js
@@ -0,0 +1,69 @@
+export function formatXml(xmlgen) {
+  return xmlFormatter(xmlgen, {
+    indentation: '  ', 
+    filter: (node) => node.type !== 'Comment', 
+    collapseContent: true, 
+    lineSeparator: '\n'
+  })
+}
+
+export async function fetchXML(store) {
+  try {
+    let sentences = toJS(store.sentences || []);
+    let videodata = toJS(store.videodata || {});
+    const response = await axios.post('http://localhost:3000/api/xmlgen', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        data: sentences.map((item)=>{return _.omit(item,'words')}),
+        meta: videodata,
+      },
+    });
+
+    if (response.data && response.data.xmlgen) {
+      store.updateData({...videodata, xmlgen: response.data.xmlgen},'videodata');
+    } else {
+      console.error('Error fetching XML');
+    }
+  } catch (error) {
+    console.error('Error fetching XML:', error);
+  }
+}
+
+export async function uploadFile(file, others, store) {
+  const apiUrl = "/api/upload";
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await axios.post(apiUrl, formData);
+    let newSentences = [...store.sentences];
+
+    if (response.data && response.data.files) {
+      let meta = toJS(others);
+      let activeIndex = meta && meta.index;
+      let files = response.data.files || [];
+      let newImageArr = newSentences[activeIndex]["selectedImgs"] || [];
+      let newVideoArr = newSentences[activeIndex]["selectedVids"] || [];
+      files.forEach((filedetails)=>{
+        let { type, url } = filedetails;
+        if (type == "image") {
+          newImageArr[newImageArr.length] = { src: url, link: url, url, index: newImageArr.length };
+        }
+        if(type == "video"){
+          newVideoArr[newVideoArr.length] = { src: url, link: url, url, index: newVideoArr.length };
+        }
+      })
+      newSentences[activeIndex]["selectedImgs"] = newImageArr;
+      newSentences[activeIndex]["selectedVids"] = newVideoArr;
+
+      store.updateSentences(newSentences);
+
+    } else {
+      console.error("Error uploading file");
+    }
+  } catch (error) {
+    console.error("Error uploading file:", error);
+  }
+}
